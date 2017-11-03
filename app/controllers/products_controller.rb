31dc91dd47d96abe155da3c65f453f63a4d89f2f
Scott Dixon <scott@howrefreshing.com.au>
2017-11-04 09:07:10 +1000

    Only show products belonging to current user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.where({user: current_user})
   end
 
   # GET /products/1
